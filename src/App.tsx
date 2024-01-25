7863487797e749aae1282a490090536581b11628
Stav <stav.levi@netnut.io>
2024-01-25 16:03:01 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// Improve your front-end skills by building projects Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+
+export default function App() {
+  return (
+    <main className="bg-[#d6e1ef] h-screen flex justify-center items-center">
+      <div className="w-64 h-96 bg-white border rounded-3xl"></div>
+      {/* <h1 className="text-3xl font-bold underline justify-center">Hello world!</h1> */}
+    </main>
+  );
+}
