86b4248c0803b3bae5cc6cd90c9b716bd31db256
Stav <stav.levi@netnut.io>
2024-01-26 02:22:08 +0200

    adding text in qr code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-// Improve your front-end skills by building projects Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-
-export default function App() {
+const App = () => {
   return (
-    <main className="bg-[#d6e1ef] h-screen flex justify-center items-center">
-      <div className="w-64 h-96 bg-white border rounded-3xl"></div>
-      {/* <h1 className="text-3xl font-bold underline justify-center">Hello world!</h1> */}
-    </main>
+    <div className="flex justify-center items-center h-screen bg-custom-blue">
+      <div className="bg-white p-5 rounded-lg shadow-lg text-center">
+        <img src="image-qr-code.png" alt="QR Code" className="mb-5 mx-auto" />
+        <h1 className="text-lg font-bold">
+          Improve your front-end skills by building projects
+        </h1>
+        <p className="text-gray-700 mt-3">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
   );
-}
+};
+
+export default App;
