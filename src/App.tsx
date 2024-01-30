efa089ab0e39e5ea71eb3957893af92da739566a
Stav <stav.levi@netnut.io>
2024-01-30 16:28:48 +0200

    update image

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
+import image from "./assets/image-qr-code.png";
 const App = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-custom-blue">
-      <div className="bg-white p-5 rounded-lg shadow-lg text-center">
-        <img src="image-qr-code.png" alt="QR Code" className="mb-5 mx-auto" />
-        <h1 className="text-lg font-bold">
-          Improve your front-end skills by building projects
-        </h1>
-        <p className="text-gray-700 mt-3">
-          Scan the QR code to visit Frontend Mentor and take your coding skills
-          to the next level
-        </p>
+    <main className="flex justify-center items-center h-screen bg-custom-blue">
+      <div
+        id="card"
+        className="bg-white rounded-2xl shadow-lg text-center space-y-6 p-3 max-w-72"
+      >
+        <img
+          src={image}
+          alt="QR Code"
+          height={576}
+          width={576}
+          className="rounded-xl"
+        />
+        <div className="space-y-2">
+          <h1 className="text-lg font-extrabold">
+            Improve your front-end skills by building projects
+          </h1>
+          <p className="text-sm text-gray-700">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
